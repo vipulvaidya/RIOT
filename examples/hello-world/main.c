a4f6883f5b11eaf4c6beba61582dd78a2edf9b55
Vipul Vaidya <vipulrvaidya@gmail.com>
2015-03-20 21:57:04 +0530

    Testing my first commit
    Signed-off-by: Vipul Vaidya

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -27,6 +27,7 @@ int main(void)
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+	printf("This board features a(n) %s MCU2.\n", RIOT_MCU);
 
     return 0;
 }
